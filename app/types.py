0263bd8380fe4644491a832d3a75c017bdfde812
jupiter67s <jupiter-67@vivaldi.net>
2022-10-08 05:38:23 +0300

    add simple validators

--- app/types.py
+++ app/types.py
@@ -3,10 +3,34 @@
 Наполовину...
 """
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class WalletInput(BaseModel):
     user_id: int
     private_key: str
     public_key: str
+
+    @validator('private_key', 'public_key', 'user_id')
+    def key_exists(cls, v):
+        if not v:
+            raise ValueError('Fields must be not empty')
+        return v
+
+
+class TransferMaticInput(BaseModel):
+    from_private_key: str
+    to_public_key: str
+    amount: float
+
+    @validator('from_private_key', 'to_public_key')
+    def key_exists(cls, v):
+        if not v:
+            raise ValueError('Keys must be not empty')
+        return v
+
+    @validator('amount')
+    def amount_greater_zero(cls, v):
+        if v <= 0:
+            raise ValueError('Amount must be greater than zero')
+        return v
